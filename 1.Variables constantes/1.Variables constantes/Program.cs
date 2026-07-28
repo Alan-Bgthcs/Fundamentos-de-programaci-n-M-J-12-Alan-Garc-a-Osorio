a871e8ef6659129e6a3d59bb45aec6bd6e3548a2
Alan <alan.garciao@upb.edu.co>
2026-07-28 13:35:02 -0500

    Mi primer commit

---
+++ 1.Variables constantes/1.Variables constantes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1.Variables_constantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aquí escribo el código fuente
+            string nombre = "Alan García Osorio";
+            Console.WriteLine("Bienvenido " + nombre + " al curso Fundamentos de programacón");
+        }
+    }
+}
