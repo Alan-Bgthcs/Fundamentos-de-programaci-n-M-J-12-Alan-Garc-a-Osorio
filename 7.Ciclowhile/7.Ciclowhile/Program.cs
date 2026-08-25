d54e7c02c5826623900be6c09c3ef13cc6f9f7f1
Alan <alan.garciao@upb.edu.co>
2026-08-25 13:30:25 -0500

    7.CicloWhile

---
+++ 7.Ciclowhile/7.Ciclowhile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Ciclowhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int contador = 1;
+            int acumulador = 1;
+
+            while (contador < 5)
+            {
+                contador++;
+                acumulador = acumulador + contador;
+            }
+
+            Console.WriteLine($" La suma se de los 5 primeros numeros es: {acumulador}");*/
+
+            int num 
+            int factorial 
+            Console.WriteLine("Digite el número del cual quiere saber el factorial");
+            num = Console.ReadLine();
+            while (contador <= num)
+            {
+                contador++;
+                acumulador = acumulador + contador;
+            }
+
+        }
+    }
+}
