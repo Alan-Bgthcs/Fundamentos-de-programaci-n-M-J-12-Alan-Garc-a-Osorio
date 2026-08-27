456544bf5486c33e2a4d4836dbd174ff876c895b
Alan <alan.garciao@upb.edu.co>
2026-08-27 12:35:47 -0500

    8.TareaCicloWhile

---
+++ 8.CiclosWhile1/8.CiclosWhile1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.CicloWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            int SumaEnteros = 0;
+
+            Console.WriteLine("ingrese un número positivo para sumar");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0)
+            {
+                SumaEnteros += numero;
+                Console.WriteLine("ingrese un número positivo para sumar");
+                numero = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("La suma de los numeros ingresados es" + SumaEnteros);
+        }
+    }
+}
