25d75992e7fc4f0da97e58803b7aa3d3365afd2e
Alan <alan.garciao@upb.edu.co>
2026-09-08 11:55:53 -0500

    14.EjercicioCicloFor

---
+++ 14.EjercicioCicloFor/14.EjercicioCicloFor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.EjercicioCicloFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dígite el número de inicio: ");
+            int inicio = int.Parse(Console.ReadLine());
+
+            Console.Write("Digíte el número de fin: ");
+            int fin = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nLos múltiplos de 5 entre {inicio} y {fin} son:");
+
+            for (int i = inicio; i <= fin; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
