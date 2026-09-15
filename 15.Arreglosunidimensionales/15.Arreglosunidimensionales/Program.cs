22ff487b392c79356c94a4ef8d6cce931ff0824b
Alan <alan.garciao@upb.edu.co>
2026-09-15 13:26:56 -0500

    15.ArreglosUnidimensionales

---
+++ 15.Arreglosunidimensionales/15.Arreglosunidimensionales/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Arreglosunidimensionales
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int [5] ;
+            numeros[0] = 10;
+            numeros[1] = 56;
+            numeros[2] = 22;
+            numeros[3] = 45;
+            numeros[4] = 102;
+
+            Console.WriteLine($"El número almacenado en la posición 3 con indice 2 es: {numeros[2]}");
+            char[] simbolos = new char[] { '&', 'r' };
+            bool[] valoresVerdad = { true, false, false, true, true, false, };
+            string[] nombres = new string[7];
+            for (int i = 0; i < 7; i++)
+            {
+                Console.WriteLine($"Ingrese el nombre para P{i + 1}: I{i}");
+                nombres[i] = Console.ReadLine();
+            }
+            Console.Clear();
+            for (int i = 0; i < nombres.Length; i++) 
+            {
+                Console.Write($"{nombres[i]}");
+            }
+        }
+    }
+}
---
+++ 15.Arreglosunidimensionales/15.Arreglosunidimensionales/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("15.Arreglosunidimensionales")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("15.Arreglosunidimensionales")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("e5f9a274-220e-4f58-a3be-c8b7bfa6bed6")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
